3296d716204c33e32f097560058cee9a261d1bbb
phuong99 <phuongdkyt@gmail.com>
2020-12-18 11:36:21 +0700

    --Phương-- sử lý thêm bài test theo với user và thêm bài test với câu hỏi , sử lý thông báo chấm điểm và kết quả bài làm(task)

--- src/main/java/com/example/demo/repository/ResultRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.entity.ResultEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ResultRepository extends JpaRepository<ResultEntity,Integer> {
-}
